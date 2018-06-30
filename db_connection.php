9e096deb413f865446e3a3b524818e1e46afc729
parveenpaul <paulparveen01@gmail.com>
2018-06-30 21:08:02 +0530

    Delete db_connection.php

--- db_connection.php
+++
@@ -1,9 +0,0 @@
-<?php 
-	$conn = mysqli_connect("localhost","root","","shareabhi0299");
-
-	// Check connection
-	if (mysqli_connect_errno())
-	  {
-	  echo "Failed to connect to DB: " . mysqli_connect_error();
-	  }
-?>
